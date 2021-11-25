c5143dfca794ecc96d592ade77d779047ce99bfd
AmineFjr <amine.f@dalvin.fr>
2021-11-25 13:01:15 +0100

    Gestion des réservations (base)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use App\Entity\Booking;
 use App\Entity\Image;
 use App\Entity\Role;
 use App\Entity\User;
@@ -25,11 +26,9 @@ class AppFixtures extends Fixture
     {
 
         $faker = Factory::create('fr-FR');
-
         $adminRole = new Role();
         $adminRole->setTitle('ROLE_ADMIN');
         $manager->persist($adminRole);
-
 
         $adminUser = new User();
         $adminUser->setFirstName('Amine')
@@ -42,20 +41,19 @@ class AppFixtures extends Fixture
             ->addUserRole($adminRole);
 
         $manager->persist($adminUser);
+
         //Nous gérons les utilisateurs
         $users = [];
         $genres = ['male','female'];
 
         for($i=1;$i<=10;$i++){
+
             $user = new User();
 
             $genre = $faker->randomElement($genres);
-
             $picture = 'https://randomuser.me/api/portraits/';
             $pictureId = $faker->numberBetween(1,99) . '.jpg';
-
             $picture .= ($genre == 'male' ? 'men/' : 'women/').$pictureId;
-
             $hash = $this->encoder->encodePassword($user, 'password');
 
             $user->setFirstName($faker->firstname($genres))
@@ -68,20 +66,18 @@ class AppFixtures extends Fixture
 
             $manager->persist($user);
             $users[] = $user;
-
         }
 
         //Nous gérons les annonces
         for($i = 1; $i <= 30; $i++) {
+
             $ad = new Ad();
 
             $title = $faker->sentence();
             $coverImage = $faker->imageUrl(1000,350);
             $introduction = $faker->paragraph(2);
             $content = '<p>' . join('<p></p>',$faker->paragraphs(5)) . '</p>';
-
             $user = $users[mt_rand(0,count($users) - 1)];
-
 
             $ad->setTitle($title)
                 ->setCoverImage($coverImage)
@@ -100,6 +96,30 @@ class AppFixtures extends Fixture
                 $manager->persist($image);
             }
 
+            //Gestion des réservations
+            for($j=1; $j<= mt_rand(0,10) ; $j++ ){
+
+                $booking = new Booking();
+                $createdAt = $faker->dateTimeBetween('-6 months');
+                $startDate = $faker->dateTimeBetween('-3 months');
+                $duration = mt_rand(3,10);
+                $endDate = (clone $startDate)->modify("+$duration days");
+                $amount = $ad->getPrice()*$duration;
+                $booker = $users[mt_rand(0,count($users)-1)];
+                $comment = $faker->paragraph();
+
+                $booking->setBooker($booker)
+                    ->setAd($ad)
+                    ->setStartDate($startDate)
+                    ->setEndDate($endDate)
+                    ->setCreatedAt($createdAt)
+                    ->setAmount($amount)
+                    ->setComment($comment);
+
+                $manager->persist($booking);
+
+
+            }
             $manager->persist($ad);
         }
 
