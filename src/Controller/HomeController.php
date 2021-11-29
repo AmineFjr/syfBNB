a344122029f941e9b4ec563b8d25eb01885a6244
AmineFjr <amine.f@dalvin.fr>
2021-11-29 16:26:13 +0100

    home(base)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller; 
+use App\Repository\AdRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,26 +10,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     
-    /**
-     *
-     * @Route("/hello/{prenom}/age/{age}", name="hello")
-     * @Route("/hello", name="hello_base")
-     * 
-     */
-    public function hello($prenom = "anonyme",  $age="0")
-    {
-        return new Response("Bonjour " . $prenom . " a " . $age);
-    }
-        
+
     /**
      * @Route("/",name="homepage")
      */
-    public function home():Response
+    public function home(AdRepository $adRepo):Response
     {
-        return $this->render('home.html.twig',['title' => "Bonjour à tous",'age' => 22]);
+        return $this->render('home.html.twig',['ads' => $adRepo->findBestAds(4)]);
     }
 
 
 }
 
-?>
